854aac4e01af805a732b3451c2cde3dfac3b356c
maxgrossenbacher <grossenbacher.max@gmail.com>
2017-09-27 10:53:49 -0600

    Update bug in code

--- sentiment_model.py
+++ sentiment_model.py
@@ -10,38 +10,36 @@ def classifer(model, X, y, name):
     X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y)
     model.fit(X_train, y_train)
     score = model.score(X_test, y_test)
-    probabilities = model.predict_proba(X_train)
+    proba = model.predict_proba(X_train)
     with open(name, 'wb') as f:
         pickle.dump(model, f)
     print('model' + name +  'accuracy score:',score)
-    print('model' + name + 'probabilites:', probabilites)
-    return score, probabilities
+    print('model' + name + 'probabilites:', proba)
+    return score, proba
 
 if __name__ == '__main__':
+    df = pd.read_pickle("../pkl_data/rest_text_target_W_ids_df.pkl")
 
+    print('sampling...')
+    df.sample(frac=1)
+    df2 = df.iloc[list(range(1000000))]
+    nlp = NlpTopicAnalysis(df2, textcol='text')
 
-df = pd.read_pickle("../pkl_data/rest_text_target_W_ids_df.pkl")
+    print('processing...')
+    nlp.process_text('../pkl_data', filename='corpus_gdc')
 
-print('sampling...')
-df.sample(frac=1)
-df2 = df.iloc[list(range(1000000))]
-nlp = NlpTopicAnalysis(df2, textcol='text')
+    # nlp = NlpTopicAnalysis()
+    # nlp.load_corpus('/Users/gmgtex/Desktop/Galvanize/Immersive/capstone/pkl_data', filename='corpus_gdc')
 
-print('processing...')
-nlp.process_text('../pkl_data', filename='corpus_gdc')
+    print('vectorizing...')
+    tfidf = nlp.vectorize(weighting='tfidf')
 
-# nlp = NlpTopicAnalysis()
-# nlp.load_corpus('/Users/gmgtex/Desktop/Galvanize/Immersive/capstone/pkl_data', filename='corpus_gdc')
+    gd = GradientBoostingClassifier()
 
-print('vectorizing...')
-tfidf = nlp.vectorize(weighting='tfidf')
-
-gd = GradientBoostingClassifier()
-
-score1, probabilities1 = classifer(gd, tfidf.toarray(), df2['target'], name='target_model')
-score2, probabilities2 = classifer(gd, tfidf.toarray(), df2['sentiment'], name='sentiment_model')
-score3, probabilities3 = classifer(gd, tfidf.toarray(), df2['starsrev'], name='rating_model')
-score4, probabilities4 = classifer(gd, tfidf.toarray(), df2['useful'], name='useful_model')
-score5, probabilities5 = classifer(gd, tfidf.toarray(), df2['usefulness'], name='usefulness_model')
-score6, probabilities6 = classifer(gd, tfidf.toarray(), df2['RestaurantsPriceRange2'], name='price_model')
-print('Done.')
+    score1, probabilities1 = classifer(gd, tfidf.toarray(), df2['target'], name='target_model')
+    score2, probabilities2 = classifer(gd, tfidf.toarray(), df2['sentiment'], name='sentiment_model')
+    score3, probabilities3 = classifer(gd, tfidf.toarray(), df2['starsrev'], name='rating_model')
+    score4, probabilities4 = classifer(gd, tfidf.toarray(), df2['useful'], name='useful_model')
+    score5, probabilities5 = classifer(gd, tfidf.toarray(), df2['usefulness'], name='usefulness_model')
+    score6, probabilities6 = classifer(gd, tfidf.toarray(), df2['RestaurantsPriceRange2'], name='price_model')
+    print('Done.')
