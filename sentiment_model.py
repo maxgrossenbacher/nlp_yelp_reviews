71e2c99951d18db9055bbc06439ba83a37748691
maxgrossenbacher <grossenbacher.max@gmail.com>
2017-09-27 10:50:06 -0600

    created classifer function to run many models at once

--- sentiment_model.py
+++ sentiment_model.py
@@ -6,6 +6,20 @@ from sklearn.ensemble import GradientBoostingClassifier
 import pickle
 
 
+def classifer(model, X, y, name):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y)
+    model.fit(X_train, y_train)
+    score = model.score(X_test, y_test)
+    probabilities = model.predict_proba(X_train)
+    with open(name, 'wb') as f:
+        pickle.dump(model, f)
+    print('model' + name +  'accuracy score:',score)
+    print('model' + name + 'probabilites:', probabilites)
+    return score, probabilities
+
+if __name__ == '__main__':
+
+
 df = pd.read_pickle("../pkl_data/rest_text_target_W_ids_df.pkl")
 
 print('sampling...')
@@ -21,48 +35,12 @@ nlp.process_text('../pkl_data', filename='corpus_gdc')
 
 print('vectorizing...')
 tfidf = nlp.vectorize(weighting='tfidf')
-X_train, X_test, y_train, y_test = train_test_split(tfidf.toarray(), df2['target'], stratify=df2['target'])
-X_train2, X_test2, y_train2, y_test2 = train_test_split(tfidf.toarray(), df2['sentiment'], stratify=df2['sentiment'])
-X_train3, X_test3, y_train3, y_test3 = train_test_split(tfidf.toarray(), df2['starsrev'], stratify=df2['starsrev'])
-X_train4, X_test4, y_train4, y_test4 = train_test_split(tfidf.toarray(), df2['useful'], stratify=df2['useful'])
 
 gd = GradientBoostingClassifier()
-gd2 = GradientBoostingClassifier()
-gd3 = GradientBoostingClassifier()
 
-print('fitting...')
-gd_model = gd.fit(X_train, y_train)
-gd2_model =gd2.fit(X_train2, y_train2)
-gd3_model =gd3.fit(X_train3, y_train3)
-gd4_model =gd4.fit(X_train4, y_train4)
-
-print('scoring')
-print('model accuracy score:',gd_model.score(X_test, y_test))
-print('model probabilites:', gd_model.predict_proba(X_test))
-
-print('model2 accuracy score:', gd2_model.score(X_test2, y_test2))
-print('model2 probabilites:', gd2_model.predict_proba(X_test2))
-
-print('model3 accuracy score:', gd3_model.score(X_test3, y_test3))
-print('model3 probabilites:', gd3_model.predict_proba(X_test3))
-
-print('model4 accuracy score:', gd4_model.score(X_test4, y_test4))
-print('model4 probabilites:', gd4_model.predict_proba(X_test4))
-
-print('pickling...')
-with open('gd_model.pkl', 'wb') as f:
-    pickle.dump(gd_model, f)
-
-print('pickling...')
-with open('gd2_model.pkl', 'wb') as f:
-    pickle.dump(gd2_model, f)
-
-print('pickling...')
-with open('gd3_model.pkl', 'wb') as f:
-    pickle.dump(gd3_model, f)
-
-print('pickling...')
-with open('gd4_model.pkl', 'wb') as f:
-    pickle.dump(gd4_model, f)
-
+score1, probabilities1 = classifer(gd, tfidf.toarray(), df2['target'], name='target_model')
+score2, probabilities2 = classifer(gd, tfidf.toarray(), df2['sentiment'], name='sentiment_model')
+score3, probabilities3 = classifer(gd, tfidf.toarray(), df2['starsrev'], name='rating_model')
+score4, probabilities4 = classifer(gd, tfidf.toarray(), df2['useful'], name='useful_model')
+score5, probabilities5 = classifer(gd, tfidf.toarray(), df2['usefulness'], name='usefulness_model')
 print('Done.')
