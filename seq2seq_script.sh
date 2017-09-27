b03dd6a3ab56e2317c47034f9b8369fedec1d47a
maxgrossenbacher <grossenbacher.max@gmail.com>
2017-09-27 11:49:40 -0600

    adding bash script to run seq2seq net

---
+++ seq2seq_script.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+VOCAB_SOURCE=${HOME}/nmt_data/toy_reverse/train/vocab.sources.txt
+VOCAB_TARGET=${HOME}/nmt_data/toy_reverse/train/vocab.targets.txt
+TRAIN_SOURCES=${HOME}/nmt_data/toy_reverse/train/sources.txt
+TRAIN_TARGETS=${HOME}/nmt_data/toy_reverse/train/targets.txt
+DEV_SOURCES=${HOME}/nmt_data/toy_reverse/dev/sources.txt
+DEV_TARGETS=${HOME}/nmt_data/toy_reverse/dev/targets.txt
+
+DEV_TARGETS_REF=${HOME}/nmt_data/toy_reverse/dev/targets.txt
+TRAIN_STEPS=1000
+
+MODEL_DIR=${TMPDIR:-/tmp}/nmt_tutorial
+PRED_DIR=${MODEL_DIR}/pred
+
+
+python -m bin.train \
+  --config_paths="
+      ./example_configs/nmt_small.yml,
+      ./example_configs/train_seq2seq.yml,
+      ./example_configs/text_metrics_bpe.yml" \
+  --model_params "
+      vocab_source: $VOCAB_SOURCE
+      vocab_target: $VOCAB_TARGET" \
+  --input_pipeline_train "
+    class: ParallelTextInputPipeline
+    params:
+      source_files:
+        - $TRAIN_SOURCES
+      target_files:
+        - $TRAIN_TARGETS" \
+  --input_pipeline_dev "
+    class: ParallelTextInputPipeline
+    params:
+       source_files:
+        - $DEV_SOURCES
+       target_files:
+        - $DEV_TARGETS" \
+  --batch_size 32 \
+  --train_steps $TRAIN_STEPS \
+  --output_dir $MODEL_DIR
+
+python -m bin.infer \
+  --tasks "
+    - class: DecodeText" \
+  --model_dir $MODEL_DIR \
+  --input_pipeline "
+    class: ParallelTextInputPipeline
+    params:
+      source_files:
+        - $DEV_SOURCES" \
+  > ${PRED_DIR}/predictions.txt
+
+python -m bin.infer \
+  --tasks "
+    - class: DecodeText
+    - class: DumpBeams
+      params:
+        file: ${PRED_DIR}/beams.npz" \
+  --model_dir $MODEL_DIR \
+  --model_params "
+    inference.beam_search.beam_width: 5" \
+  --input_pipeline "
+    class: ParallelTextInputPipeline
+    params:
+      source_files:
+        - $DEV_SOURCES" \
+  > ${PRED_DIR}/predictions.txt
