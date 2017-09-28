0efcb57e6516f7961387b19d57816670dd386b0d
maxgrossenbacher <grossenbacher.max@gmail.com>
2017-09-28 14:58:39 -0600

    more

--- seq2seq_script.sh
+++ seq2seq_script.sh
@@ -69,20 +69,20 @@ python -m bin.infer \
         - $DEV_SOURCES" \
   > ${PRED_DIR}/predictions.txt
 
-python -m bin.infer \
-  --tasks "
-    - class: DecodeText
-    - class: DumpBeams
-      params:
-        file: ${PRED_DIR}/beams.npz" \
-  --model_dir $MODEL_DIR \
-  --model_params "
-    inference.beam_search.beam_width: 5" \
-  --input_pipeline "
-    class: ParallelTextInputPipeline
-    params:
-      source_files:
-        - $DEV_SOURCES" \
-  > ${PRED_DIR}/predictions.txt
+# python -m bin.infer \
+#   --tasks "
+#     - class: DecodeText
+#     - class: DumpBeams
+#       params:
+#         file: ${PRED_DIR}/beams.npz" \
+#   --model_dir $MODEL_DIR \
+#   --model_params "
+#     inference.beam_search.beam_width: 5" \
+#   --input_pipeline "
+#     class: ParallelTextInputPipeline
+#     params:
+#       source_files:
+#         - $DEV_SOURCES" \
+#   > ${PRED_DIR}/predictions.txt
 
   ./bin/tools/multi-bleu.perl ${DEV_TARGETS_REF} < ${PRED_DIR}/predictions.txt
