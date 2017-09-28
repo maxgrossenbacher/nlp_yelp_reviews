9a125689ac0f36c80c9e72b4f997b21649796610
maxgrossenbacher <grossenbacher.max@gmail.com>
2017-09-28 17:36:00 -0600

    maybe this time?

--- seq2seq_script.sh
+++ seq2seq_script.sh
@@ -27,7 +27,7 @@ DEV_SOURCES=${TEXT_DIR}/train_text.txt
 DEV_TARGETS=${TEXT_DIR}/train_label.txt
 
 # DEV_TARGETS_REF=${TEXT_DIR}/train_label.txt
-TRAIN_STEPS=5000
+TRAIN_STEPS=1000
 
 MODEL_DIR=${HOME}/models
 PRED_DIR=${MODEL_DIR}/pred
