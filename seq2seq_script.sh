3c026f6ee7030c25c3ae020949209ab0ee29fbe1
maxgrossenbacher <grossenbacher.max@gmail.com>
2017-09-28 16:15:45 -0600

    Update debuggin maybe finally figured it out

--- seq2seq_script.sh
+++ seq2seq_script.sh
@@ -73,21 +73,21 @@ python -m bin.infer \
         - $DEV_SOURCES" \
   > ${PRED_DIR}/predictions.txt
 
-python -m bin.infer \
-  --tasks "
-    - class: DecodeText
-    - class: DumpBeams
-      params:
-        file: ${PRED_DIR}/beams.npz" \
-  --model_dir $MODEL_DIR \
-  --model_params "
-    inference.beam_search.beam_width: 5" \
-  --input_pipeline "
-    class: ParallelTextInputPipeline
-    params:
-      source_files:
-        - $DEV_SOURCES" \
-  > ${PRED_DIR}/predictions.txt
+# python -m bin.infer \
+#   --tasks "
+#     - class: DecodeText
+#     - class: DumpBeams
+#       params:
+#         file: ${PRED_DIR}/beams.npz" \
+#   --model_dir $MODEL_DIR \
+#   --model_params "
+#     inference.beam_search.beam_width: 5" \
+#   --input_pipeline "
+#     class: ParallelTextInputPipeline
+#     params:
+#       source_files:
+#         - $DEV_SOURCES" \
+  # > ${PRED_DIR}/predictions.txt
 
   # python -m bin.tools.generate_beam_viz  \
   # -o ${TMPDIR:-/tmp}/beam_visualizations \
