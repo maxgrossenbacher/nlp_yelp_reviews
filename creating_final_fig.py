182a3d596f365025017d6282372e3c55ffb3dace
maxgrossenbacher <grossenbacher.max@gmail.com>
2017-10-05 09:23:18 -0600

    update final figure

--- creating_final_fig.py
+++ creating_final_fig.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 plt.style.use('ggplot')
-df = pd.DataFrame({'Mutlinomial Naive Bayes':[0.445, 0.542, 0.342], 'Opt. Models TF-IDF':[0.579,0.676,0.439], 'Opt. Models Doc2Vec':[0.581,0.724,0.493], 'Target':['usefulness','sentiment','rating']})
+df = pd.DataFrame({'Mutlinomial Naive Bayes':[0.445, 0.542, 0.342], 'Opt. Models TF-IDF':[0.579,0.676,0.439], 'Opt. Models Doc2Vec':[0.581,0.724,0.500], 'Target':['usefulness','sentiment','rating']})
 ax = df.plot.barh(title='Weighted F1 Score', x='Target', style=['fivethirtyeight'],figsize=(11,7), fontsize=17)
 plt.legend(bbox_to_anchor=(1,.8), fontsize=15)
 plt.tight_layout()
